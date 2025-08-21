119d9fba59a552145001f8fd4f64038b568a2a4b
Rainy5531 <hrishikesh.batchu@gmail.com>
2025-08-21 18:44:45 +0530

    initial commit

---
+++ Module 8/Classwork/Lesson 1/L1 A4.py
@@ -0,0 +1,12 @@
+#Print a Sentence
+print("Welcome to the world of programming!")
+
+#print a number
+print(4)
+
+#Next Line after print
+print("Hello Python \n\n\nThis sentence is coming in new line")
+
+#Print all the keywords present in Python print("Keywords in python are....")
+import keyword
+print(keyword.kwlist)
