13769941ae79cbff0820f7b25f6dc3b2246cd253
Rainy5531 <hrishikesh.batchu@gmail.com>
2025-08-26 18:58:07 +0530

    initial commit

---
+++ Module 8/Classwork/Lesson 2/L2 A4.py
@@ -0,0 +1,14 @@
+x = input("Enter value of x:")
+y = input("Enter value of y:")
+
+#Swapping
+temp = x #temp = 23, x = 23, y = 78
+x = y   #temp = 23, x = 78, y = 78
+y = temp #temp = 23, x = 78, y = 23
+
+#Single line swap
+#x,y = y,x
+
+#Displaying results after swapping
+print("Value of x after swapping:", x)
+print("Value of y after swapping:", y)
