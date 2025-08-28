9c91ae15d095e2236251b70f7720bc730826ea4b
Rainy5531 <hrishikesh.batchu@gmail.com>
2025-08-28 18:36:55 +0530

    initial commit

---
+++ Module 8/Classwork/Lesson 3/L3 A4.py
@@ -0,0 +1,11 @@
+import datetime, calendar
+
+
+# using now() to get current time
+current_time = datetime.datetime.today()
+
+# Printing value of now
+print("Time now in India is :", current_time)
+
+# print calendar of year 2021
+print("\n", calendar.calendar(2021))
