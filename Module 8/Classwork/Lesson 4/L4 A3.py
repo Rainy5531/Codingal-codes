529e20814a9aa9060bbefa851b37d5a060b59efd
Rainy5531 <hrishikesh.batchu@gmail.com>
2025-09-02 18:32:35 +0530

    initial commit

---
+++ Module 8/Classwork/Lesson 4/L4 A3.py
@@ -0,0 +1,9 @@
+num = 1
+sum = 0
+
+while(num <= 10):
+    sum += num # 1, 3, 6, 10, 15, 21, 28, 36, 45, 55
+    num += 1 # 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+
+print("Sum of First 10 Natural Numbers :", sum)
+#1+2+3+...+10 = 55
