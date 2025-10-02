f32b5151a81b384274c60bf1984857c2e33a5458
Rainy5531 <hrishikesh.batchu@gmail.com>
2025-10-02 18:33:14 +0530

    intial commit

---
+++ Module 10/Classwork/Lesson 1/L1 A2.py
@@ -0,0 +1,26 @@
+# open the file in read mode
+file_read = open('Codingal2.txt', 'r')
+print("File in Read Mode -")
+print(file_read.read())
+file_read.close()
+
+# open the file in write mode
+file_write = open('Codingal2.txt', 'w')
+# write in the file
+file_write.write(" File in write mode ....")
+file_write.write("Hi! I am Penguin. I am 1 yr. old.")
+file_write.close()
+
+# open the file in append mode
+file_append = open('Codingal2.txt', 'a')
+# append in the file
+file_append.write("\n File in append mode ....")
+file_append.write("Hi! I am Penguin. I am 1 yr. old.")
+file_append.close()
+
+'''
+ Original text in Codingal2.txt
+ -------------------------------
+Codingal is on a mission to inspire school kids to fall in love with coding.
+While still in school, those who start young will be ahead of everyone by the time they get into college.
+'''
