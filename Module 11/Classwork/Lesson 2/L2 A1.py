3af4c004aba9d1cfb3c990f823459575eb1fcbaf
Rainy5531 <hrishikesh.batchu@gmail.com>
2025-10-30 18:39:31 +0530

    initial commit

---
+++ Module 11/Classwork/Lesson 2/L2 A1.py
@@ -0,0 +1,11 @@
+# Program to show constant time complexity
+
+def printnumber(n):
+    iteration = 0
+    print("The number entered by user is ", n)
+    iteration += 1
+    print("The number of iterations done by the code is ", iteration, "\n")
+
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' the time taken by our code won't change")
---
+++ Module 11/Classwork/Lesson 2/L2 A2.py
@@ -0,0 +1,9 @@
+def OnTime(n):
+    iteration = 0
+    for i in range(0, n):
+        iteration += 1
+    print("When n is", n, "iterations =", iteration)
+
+OnTime(10)
+OnTime(20)
+OnTime(42)
