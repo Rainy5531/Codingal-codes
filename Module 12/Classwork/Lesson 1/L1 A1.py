f8e8a91ce024bdb32d7578a22b6e38cafc16cf8b
Rainy5531 <hrishikesh.batchu@gmail.com>
2025-11-18 18:33:03 +0530

    initial commit

---
+++ Module 12/Classwork/Lesson 1/L1 A1.py
@@ -0,0 +1,40 @@
+num1 = 10
+num2 = 4
+
+print("num1 & num2 are:", num1, num2)
+print("num1 | num2 =", num1 | num2) #OR operator
+print("num1 & num2 =", num1 & num2) #AND operator
+print("num1 ^ num2 =", num1 ^ num2) #XOR operator
+print("num1 << 2 =", num1 << 2) #Left Shift operator
+print("num2 >> 2 =", num2 >> 2) #Right Shift operator
+print("~num1 =", ~num1) #negation operator
+print("~num2 =", ~num2)
+
+'''
+10 = 1010
+4 = 0100
+10 | 4 = 14 -> 1010 | 0100 = 1110
+10 & 4 = 0  -> 1010 & 0100 = 000
+10 ^ 4 = 14 -> 1010 ^ 0100 = 1110
+10 << 4 = 160 -> 1010 << 4 = 10100000
+10 >> 4 = 0  -> 1010 >> 4 = 0000
+
+
+1 or 1 = 1 \
+1 or 0 = 1  \
+0 or 1 = 1  / OR operator
+0 or 0 = 0 /
+
+1 AND 1 = 1 \
+1 AND 0 = 0  \
+0 AND 1 = 0  / AND operator
+0 AND 0 = 0 /
+
+1 XOR 1 = 0 \
+0 XOR 0 = 0  \
+1 XOR 0 = 1  / XOR operator
+0 XOR 1 = 1 /
+
+not 1 = 0 \
+not 0 = 1 / Negation operator
+'''
