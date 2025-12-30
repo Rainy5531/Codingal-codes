7324f4e6c71b69818d55133ebad0f1e93af390e1
Rainy5531 <hrishikesh.batchu@gmail.com>
2025-12-30 18:41:13 +0530

    initial commit

---
+++ Module 14/Classwork/Lesson 1/L1 A1.py
@@ -0,0 +1,18 @@
+print("What is the type of the given above database -\n 1)Relational Database \n 2)Non-Relational Database")
+answer = int(input("Enter your guess here..."))
+if answer == 2:
+    print("You guessed it right!")
+else:
+    print("Unfortunately your guess was wrong.")
+
+print("\nPlease tell your mentor why you guessed this?\n\n")
+
+
+print("What is the type of the given above database -\n 1)Relational Database \n 2)Non-Relational Database")
+answer = int(input("Enter your guess here..."))
+if answer == 1:
+    print("You guessed it right!")
+else:
+    print("Unfortunately your guess was wrong.")
+
+print("\nPlease tell your mentor why you guessed this?")
