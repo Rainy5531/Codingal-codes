fc3b91012586d3d4e6be8f004dd24e88398b30ec
Rainy5531 <hrishikesh.batchu@gmail.com>
2026-01-08 18:40:10 +0530

    initial commit

---
+++ Module 14/Classwork/Lesson 4/L4 A2.py
@@ -0,0 +1,57 @@
+# Connect with sqlite database
+# Import necessary libraries
+import pandas as pd
+import sqlite3
+
+# Datebase file
+database = 'database.sqlite'
+
+# Connect to the database
+conn = sqlite3.connect(database)
+print("Opened database successfullt")
+
+# --------------------------------------------------
+# Display all the tables of the database
+df = pd.read_sql("""
+                 SELECT *
+                 FROM sqlite_master
+                 WHERE type = 'table'
+""", conn)
+
+# --------------------------------------------------
+# Display the first five rows of the Player_match table
+player_match = pd.read_sql("""
+                            SELECT *
+                           FROM Player_Match
+""", conn)
+
+print(player_match.head())
+
+# -----------------------------------------------------
+# Check the presence of NULL values in the Player_match table
+null_player_match = pd.read_sql("""
+                            SELECT *
+                           FROM Player_Match
+                           WHERE Team_Id IS NULL
+""", conn)
+
+print(null_player_match)
+
+# ---------------------------------------------------------
+# Display the first five rows of the Match table
+toss_dec = pd.read_sql("""
+                        SELECT *
+                        FROM Match
+""", conn)
+
+print(toss_dec.head())
+
+# -------------------------------------------------------------
+# Check the presence of NULL values in the Match table
+null_match = pd.read_sql("""
+                         SELECT *
+                         FROM Match
+                         WHERE Match_Winner IS NULL
+""", conn)
+
+print(null_match)
